67372333e2729b73c821942be18940294e4236ea
Cupcake416 <2573865855@qq.com>
2021-06-05 18:51:52 +0800

    add cache manager

---
+++ CacheManager/CacheManager.cpp
@@ -0,0 +1,142 @@
+//
+// Created by gwy on 2021/6/5.
+//
+
+#include <sys/stat.h>
+#include "CacheManager.h"
+
+using namespace std;
+
+CacheManager::CacheManager() {
+    pages.resize(MaxPages);
+    maxLRU = 0;
+    int id = 0;
+    for(auto &page : pages) {
+        page.id = id;
+    }
+}
+
+int CacheManager::getPageTail(string filename) {
+    struct stat st;
+    if (stat(filename.c_str(), &st) == 0) {
+        return (st.st_size / BlockSize - 1);
+    }
+    cerr << "Failed to get file tail" << endl;
+}
+
+void CacheManager::setDirty(const string &filename, unsigned int pageID) {
+    Page& page = findPagePair(filename, pageID);
+    page.dirty = true;
+}
+
+void CacheManager::setValid(int id) {
+    maxLRU++;
+    Page& page = pages[id];
+    page.valid = true;
+    page.LRUCnt = maxLRU;
+}
+
+Page& CacheManager::getLRU() {
+    int minLRU = maxLRU;
+    Page* minPage = nullptr;
+    for(auto page: pages) {
+        if(page.valid) continue;
+        if(page.LRUCnt < minLRU) {
+            minLRU = page.LRUCnt;
+            minPage = const_cast<Page *>(&page);
+        }
+    }
+    if (minPage == nullptr) { cerr << "No LRU page found!"; }
+    return *minPage;
+}
+
+void CacheManager::removeFile(string filename) {
+    for(auto &page: pages) {
+        if(page.filename == filename) page.reset()
+    }
+
+    if(remove(filename.c_str())) {
+        cerr << "Fail to remove file: " << filename << endl;
+    }
+}
+
+void CacheManager::createFile(string in) {
+    ofstream f1(in);
+}
+
+Page& CacheManager::getFreePage() {
+    for (auto &page: pages) {
+        if (!page.dirty && !page.vaild) {
+            page.reset();
+            setValid(page.id);
+            return page;
+        }
+    }
+
+    Page &p = getLRU();
+    p.flush().reset();
+    setValid(p.id);
+
+    return p;
+}
+
+char* CacheManager::getPage(string filename, unsigned int pageID, bool allocate) {
+    for (auto &page: pages) {
+        if (page.filename == filename && page.pageID == pageID) {
+            setValid(page.id);
+            pageMap.insert(PageMap::value_type(make_pair(filename, pageID), page));
+            return page.content;
+        }
+    }
+
+    fstream fp;
+    fp.open(filename, ios::in | ios::out | ios::binary);
+    if (!fp.good())
+        cerr << "Fail to open file: " << filename << "." << endl;
+    fp.seekg(ios_base::end);
+    int pageOffset = getPageTail(filename) + 1;
+//    cout << "Detected pageOffset: " << pageOffset << endl;
+    if (pageID >= pageOffset) {
+        if (!allocate) return nullptr;
+        if (pageOffset != pageID) {
+            cerr << fp.tellg() << " " << pageOffset << " " << pageID << endl;
+            cerr << "Requesting way beyond the tail!" << endl;
+            return nullptr;
+        }
+//        cout << "Requesting new page..." << endl;
+    }
+
+    Page &page = getFreePage();
+    page.assign(filename, pageID);
+    pageMap.insert(BlockMap::value_type(make_pair(filename, pageID), page));
+
+    fp.seekg(offset * PageSize, ios::beg);
+    fp.read(page.content, PageSize);
+    fp.close();
+    setValid(page.id);
+    page.flush();
+    return page.content;
+}
+
+void CacheManager::setFree(string filename, unsigned int pageID) {
+    Page &page = findPagePair(filename, pageID);
+    page.valid = false;
+    pageMap.erase(make_pair(filename, page.pageID));
+}
+
+Page& CacheManager::findPagePair(string filename, unsigned int pageID) const {
+    auto parent = blockMap.find(make_pair(filename, pageID));
+    if (parent == pageMap.end()) {
+        cerr << "Element not found!";
+    }
+    return parent->second;
+}
+
+void CacheManager::flushAllPages() {
+    for (auto &page : pages) {
+        if (page.dirty) {
+            page.flush();
+        }
+        page.reset();
+    }
+}
---
+++ CacheManager/CacheManager.h
@@ -0,0 +1,94 @@
+//
+// Created by gwy on 2021/6/5.
+//
+
+#ifndef MINISQL_CACHEMANAGER_H
+#define MINISQL_CACHEMANAGER_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdio>
+#include <map>
+#include <utility>
+#include "../Base/Base.h"
+
+using namespace std;
+using namespace MINISQL_BASE;
+
+struct Page {
+    string filename;
+    int id;
+    unsigned int pageID;
+    bool valid;
+    bool dirty;
+    int LRUCnt;
+    char content[PageSize];
+
+    Page() {
+        reset();
+    }
+
+    void reset() {
+        vaild = dirty = false;
+        memset(content, 0, PageSize);
+    }
+
+    void assign(string filename, int pageID) {
+        this->fileName = filename;
+        this->pageID = pageID;
+    }
+
+    Page& flush() {
+        fstream fp;
+        fp.open(filename, ios::in | ios::out | ios::ate | ios::binary);
+        fp.seekg(pageID * PageSize, ios::beg);
+//        cout << "Flushing: " << content << endl;
+        fp.write(content, PageSize);
+        fp.close();
+        return *this;
+    }
+};
+
+class CacheManager {
+
+public:
+    CacheManager();
+
+    ~CacheManager() = default;
+
+    int getPageTail(string filename);
+
+    void setDirty(const string &filename, unsigned int pageID);
+    void setFree(string filename, unsigned int pageID);
+
+    char *getPage(string filename, unsigned int pageID, bool allocate = false);
+
+    void flushAllPages(); // write all content in page to disk
+
+    void createFile(string);
+
+    Page& getLRU();
+
+    void removeFile(string filename);
+
+
+
+private:
+    typedef map<pair<string, unsigned int>, Block &> PageMap;
+
+    PageMap pageMap;
+
+    std::vector<Page> pages;
+
+    void setValid(int id); // mark as using
+
+    // find page id which is available
+    Page &getFreePage();
+
+    Page &findPagePair(string filename, unsigned int pageID) const;
+
+    int maxLRU;
+};
+
+#endif //MINISQL_CACHEMANAGER_H
