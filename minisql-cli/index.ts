5b142e456973c702bd88ac59ab14f64e21a4f989
enzymii <enzymiix@gmail.com>
2022-05-27 15:13:18 +0800

    fix: add try block in cli

--- minisql-cli/index.ts
+++ minisql-cli/index.ts
@@ -11,39 +11,41 @@ type Answer = {
 (async () => {
   const client = await MiniSQLClient.create();
   while (true) {
-    const { sql, table, isSelect } = await Inquirer.prompt<Answer>([
-      {
-        type: 'input',
-        name: 'sql',
-        message: 'Please input your SQL:',
-      },
-      {
-        type: 'input',
-        name: 'table',
-        message: 'Please input the table name:',
-      },
-      {
-        type: 'confirm',
-        name: 'isSelect',
-        message: 'Is this a select statement?',
-        default: false,
-      },
-    ]);
+    try {
+      const { sql, table, isSelect } = await Inquirer.prompt<Answer>([
+        {
+          type: 'input',
+          name: 'sql',
+          message: 'Please input your SQL:',
+        },
+        {
+          type: 'input',
+          name: 'table',
+          message: 'Please input the table name:',
+        },
+        {
+          type: 'confirm',
+          name: 'isSelect',
+          message: 'Is this a select statement?',
+          default: false,
+        },
+      ]);
 
-    const { code, msg } = isSelect
-      ? await client.queryRegion(table, sql)
-      : await client.queryMaster(table, sql);
+      const { code, msg } = isSelect
+        ? await client.queryRegion(table, sql)
+        : await client.queryMaster(table, sql);
 
-    if (code === 0) {
-      console.log(msg);
-    } else if (code > 0) {
-      console.log(Chalk.cyanBright(msg));
-    } else {
-      console.log(Chalk.redBright(msg));
-    }
+      if (code === 0) {
+        console.log(msg);
+      } else if (code > 0) {
+        console.log(Chalk.cyanBright(msg));
+      } else {
+        console.log(Chalk.redBright(msg));
+      }
 
-    if (sql.toLocaleLowerCase() === 'exit;') {
-      break;
-    }
+      if (sql.toLocaleLowerCase() === 'exit;') {
+        break;
+      }
+    } catch (e) {}
   }
 })();
